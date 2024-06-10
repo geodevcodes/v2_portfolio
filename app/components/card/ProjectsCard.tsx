c128250b5da92ad71d41be97f18089f588aca7c5
geodevcode <geodevinstitute@gmail.com>
2024-06-10 14:03:19 +0100

    ProjectsCard updated

--- app/components/card/ProjectsCard.tsx
+++ app/components/card/ProjectsCard.tsx
@@ -2,27 +2,47 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function ProjectsCard() {
+interface ProjectCardData {
+  cardImageUrl: string;
+  urlLink: string;
+}
+
+export default function ProjectsCard({
+  cardImageUrl,
+  urlLink,
+}: ProjectCardData) {
   return (
     <div>
-      <div className="border border-slate">
+      <div className="border border-slate rounded-md">
         <div>
-          <div className="relative w-full h-[180px] md:h-[200px] lg:h-[250px] xl:h-[300px]">
-            <Image src="/sass.webp" alt="ecommerce prototype image" fill />
+          <div className="relative rounded-t-md h-[200px] md:h-[200px] lg:h-[250px] xl:h-[300px]">
+            <Image
+              src={cardImageUrl}
+              alt="ecommerce prototype image"
+              fill
+              className="rounded-t-md object-cover w-full h-[180px]"
+            />
           </div>
           <div className="p-4 lg:p-3">
             <p className="font-semibold  text-sm">
               Create a SaaS Application with Next.js.14, Stripe, Kinde, Prisma,
               Supabase and...
             </p>
-            <div className="lg:flex justify-between gap-2 w-full">
+            <div className="w-full">
               <p className="text-muted-foreground text-sm">
-                Build a SaaS Application using Next.js 14, Stripe, Kinde,
-                Prisma, Supabase, and Tailwind! Learn step...
+                Today we are going to create a Digital Marketplace using Next.js
+                14, Stripe Connect,
               </p>
               <div>
-                <Button asChild className="w-full mt-4 lg:mt-0">
-                  <Link href="/about">Watch</Link>
+                <Button asChild className="w-full mt-4">
+                  <Link
+                    href={urlLink}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-white"
+                  >
+                    View Now
+                  </Link>
                 </Button>
               </div>
             </div>
