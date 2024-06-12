6ed75e58180862970fa0308a17ade895f0bd523e
geodevcode <geodevinstitute@gmail.com>
2024-06-12 14:08:04 +0100

    session added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from "./providers/theme-provider";
 import MobileNavbar from "./components/navbar/MobileNavbar";
 import SideBar from "./components/sidebar/Sidebar";
 import SessionProviderPage from "./providers/session-provider";
+import { getServerSession } from "next-auth";
+import { authConfig } from "./api/auth/[...nextauth]/option";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +15,13 @@ export const metadata: Metadata = {
   description: "geodevcodes Micro-frontend Application",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authConfig);
+  
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -30,7 +34,7 @@ export default function RootLayout({
           >
             <MobileNavbar />
             <section className="w-full relative flex">
-              <SideBar />
+              <SideBar  session={session}/>
               <main className="w-full p-2 pt-20 md:pt-2 max-w-[1150px] px-4 mx-auto xl:max-w-full xl:overflow-x-auto md:ml-[240px] xl:ml-[240px]">
                 {children}
               </main>
