7f0f0d06f0346fb8b545cbb32822c31759c7b69d
geodevcode <geodevinstitute@gmail.com>
2024-06-12 14:09:35 +0100

    profile component updated

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,67 @@
-export default function About() {
+import { Button } from "@/components/ui/button";
+
+import { getServerSession } from "next-auth";
+import { authConfig } from "../api/auth/[...nextauth]/option";
+
+export default async function Profile() {
+  const session = await getServerSession(authConfig);
+
+  console.log(session, "this is session ==")
+
+  const fullName = session?.user?.name
+  const email = session?.user?.email
+  // const profilePicture = session?.user?.picture
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <div className="w-full items-center justify-between font-mono text-sm lg:flex p-4">
-        <p>Hey there! this is Profile page</p>
-        <div>
-          <a href="/projects">Projects</a> |{" "}
-          <a href="/profile" className="ml-4">
-            Profile
-          </a>
-        </div>
+    <main className="min-h-screen md:px-6 lg:p-8">
+      <div className="w-full font-mono text-sm ">
+        <p className="text-2xl md:text-5xl font-bold mb-2">Settings</p>
+        <p className="text-muted-foreground">
+          Manage the settings of your account
+        </p>
       </div>
+      <section className="h-fit border border-accent-primary mt-8 p-6 rounded-md">
+        {/* ====Name === */}
+        <div>
+          <label htmlFor="name">
+            <span>Name</span>
+            <input
+              type="text"
+              placeholder="Full name"
+              value={`${fullName}`}
+              className="w-full border border-slate dark:bg-black/30 rounded-sm p-3 focus:outline-none mt-2 text-sm"
+            />
+          </label>
+        </div>
+
+        {/* ====Email === */}
+        <div className="mt-3">
+          <label htmlFor="email">
+            <span>Email</span>
+            <input
+              type="email"
+              value={`${email}`}
+              placeholder="Email"
+              disabled
+              className="w-full border border-slate dark:bg-black/30 rounded-sm p-3 focus:outline-none mt-2 text-sm text-muted-foreground cursor-not-allowed"
+            />
+          </label>
+        </div>
+        {/* ====Color Scheme === */}
+        <div className="mt-3">
+          <label htmlFor="colorScheme">
+            <span>Color Scheme</span>
+            <input
+              type="Color Scheme"
+              placeholder="Color"
+              className="w-full border border-slate dark:bg-black/30 rounded-sm p-3 focus:outline-none mt-2 text-sm"
+            />
+          </label>
+        </div>
+        <div className="mt-3">
+          <Button>Save Now</Button>
+        </div>
+      </section>
     </main>
   );
 }
