2d0f4a76f22a87b273e5345005f30ddaaf3295fe
geodevcode <geodevinstitute@gmail.com>
2024-06-21 09:54:03 +0100

    privacy page created

---
+++ app/privacy/page.tsx
@@ -0,0 +1,7 @@
+export default function Privacy() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <div className="">This is Privacy and Policy page...</div>
+    </main>
+  );
+}
