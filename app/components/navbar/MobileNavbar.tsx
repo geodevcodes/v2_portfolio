96a4ef07f126d2ea433a0ae1d231227f28bfc7d2
geodevcode <geodevinstitute@gmail.com>
2024-06-21 08:38:37 +0100

    micro changed to Rasheed Olatunde

--- app/components/navbar/MobileNavbar.tsx
+++ app/components/navbar/MobileNavbar.tsx
@@ -2,6 +2,8 @@
 import Link from "next/link";
 import { ModeToggle } from "../modeToggle/ModeToggle";
 import { MobileSidebar } from "../sidebar/MobileSidebar";
+
+
 import { usePathname } from "next/navigation";
 
 export default function MobileNavbar() {
@@ -11,7 +13,7 @@ export default function MobileNavbar() {
     <nav className="z-[1000] bg-background fixed px-[20px] py-[10px] top-0 left-0 right-0 border-b border-accent md:hidden">
       <div className="flex justify-between items-center">
         <Link href="/" className="font-bold ">
-          <span className="text-primary text-lg">Micro</span>
+          <span className="text-primary text-md">Rasheed Olatunde</span>
         </Link>
         <div className="flex justify-between gap-x-2 w-28">
           <ModeToggle />
