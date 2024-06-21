12653b06049c3342b420d0de9d24aaec31502f82
geodevcode <geodevinstitute@gmail.com>
2024-06-21 09:53:43 +0100

    terms page created

---
+++ app/terms/page.tsx
@@ -0,0 +1,7 @@
+export default function Terms() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <div className="">This is Terms and Condition page...</div>
+    </main>
+  );
+}
