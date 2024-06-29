ac50fb380821a3fd99d4ababf7c7e9c8f2c2e9d2
geodevcode <geodevinstitute@gmail.com>
2024-06-29 18:00:46 +0100

    props added and necessary styling added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,11 +5,16 @@ import ProjectsCard from "../components/card/ProjectsCard";
 export default function Projects() {
   return (
     <main className="px-4 p-2 md:p-8 min-h-screen mb-6">
-      <div className="w-full items-center font-sans gap-6 text-sm p-2 justify-between gap-y-10 lg:grid lg:grid-cols-2 xl:grid-cols-3">
+      <div className="w-full items-center font-sans gap-6 space-y-6 lg:space-y-0 text-sm p-2 justify-between gap-y-10 lg:grid lg:grid-cols-2 xl:grid-cols-3">
         {projectLinks &&
           projectLinks.map((item, idx) => (
             <div key={idx}>
-              <ProjectsCard urlLink={item.href} cardImageUrl={item.imageUrl} />
+              <ProjectsCard
+                projectId={item.id}
+                projectTitle={item.projectTitle}
+                subTitle={item.subTitle}
+                cardImageUrl={item.imageUrl}
+              />
               {/* <Skeleton /> */}
             </div>
           ))}
