56922a9473b50165043523f764a0a575a9a7da16
geodevcode <geodevinstitute@gmail.com>
2024-06-29 18:01:53 +0100

    props added,  necessary styling added and fetching single project logic added

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -2,15 +2,20 @@ import Image from "next/image";
 import { FaGithub } from "react-icons/fa";
 import { BiWorld } from "react-icons/bi";
 import Link from "next/link";
+import { projectLinks } from "@/lib/LinkData";
 
 export default function ProjectsDetailsPage({ params }: { params: any }) {
+  const projectId = params.id;
+  const projectArray = projectLinks || [];
+  const project = projectArray.find((item) => item?.id === projectId);
+
   return (
     <div className="px-4 p-2 mb-4 md:p-8 lg:mb-0  font-sans min-h-screen ">
       <div className="font-sans text-sm space-y-10 lg:space-x-6 lg:space-y-0 lg:grid lg:grid-cols-2">
         <div className="col-span-2/3 space-y-6">
           <div className="relative h-[150px] md:h-[200px] xl:h-[350px] 2xl:h-[720px]">
             <Image
-              src="/sass.webp"
+              src={(project?.imageUrl as string) ?? undefined}
               alt="project image"
               fill
               sizes="(max-width: 768px) 100vw, 700px"
@@ -19,42 +24,19 @@ export default function ProjectsDetailsPage({ params }: { params: any }) {
           </div>
           <div className="p-5 border border-accent-primary rounded-md">
             <h3 className="lg:text-3xl font-semibold">
-              Create a SaaS Application with Next.js.14, Stripe, Kinde, Prisma,
-              Supabase
+              {project?.projectTitle}
             </h3>
             <p className="text-muted-foreground text-sm mt-2">
-              Create a modern Portfolio Website with Next.js.14, Tailwind,
-              Kinde, Prisma and Supabase! Learn step-by-step and elevate your
-              development skills.
+              {project?.subTitle}
             </p>
-            <div className="mt-6 flex flex-wrap gap-x-2 gap-y-2 items-center">
-              <span className="bg-accent dark:bg-accent text-primary p-1 border border-accent-primary dark:border dark:border-primary rounded-sm text-xs">
-                Typescript
-              </span>
-              <span className="bg-accent dark:bg-accent text-primary p-1 border border-accent-primary dark:border dark:border-primary rounded-sm text-xs">
-                Nextjs
-              </span>
-              <span className="bg-accent dark:bg-accent text-primary p-1 border border-accent-primary dark:border dark:border-primary rounded-sm text-xs">
-                Kinde
-              </span>
-              <span className="bg-accent dark:bg-accent text-primary p-1 border border-accent-primary dark:border dark:border-primary rounded-sm text-xs">
-                Sanity
-              </span>
-              <span className="bg-accent dark:bg-accent text-primary p-1 border border-accent-primary dark:border dark:border-primary rounded-sm text-xs">
-                Vercel
-              </span>
-              <span className="bg-accent dark:bg-accent text-primary p-1 border border-accent-primary dark:border dark:border-primary rounded-sm text-xs">
-                Tailwind
-              </span>
-              <span className="bg-accent dark:bg-accent text-primary p-1 border border-accent-primary dark:border dark:border-primary rounded-sm text-xs">
-                Shadcn/UI
-              </span>
-              <span className="bg-accent dark:bg-accent text-primary p-1 border border-accent-primary dark:border dark:border-primary rounded-sm text-xs">
-                Superbase
-              </span>
-              <span className="bg-accent dark:bg-accent text-primary p-1 border border-accent-primary dark:border dark:border-primary rounded-sm text-xs">
-                Prisma
-              </span>
+            <div className="mt-6 flex flex-wrap gap-x-2 gap-y-3 items-center">
+              {project?.stack?.map((item, idx) => (
+                <span key={idx} className="">
+                  <span className="bg-accent dark:bg-accent text-primary p-1 border border-accent-primary dark:border dark:border-primary rounded-sm text-xs">
+                    {item}
+                  </span>
+                </span>
+              ))}
             </div>
           </div>
         </div>
@@ -65,33 +47,27 @@ export default function ProjectsDetailsPage({ params }: { params: any }) {
               Learn More about what you will learn in this free video
             </p>
             <div className="mt-6 space-y-2.5 text-[16px]">
-              <p>🌐 Next.js App Router</p>
-              <p>🔐 Kinde Authentication</p>
-              <p>📧 Passwordless Auth</p>
-              <p>🔑 OAuth (Google and GitHub)</p>
-              <p>💽 Supabase Database</p>
-              <p>💨 Prisma Orm</p>
-              <p>🎨 Styling with tailwindcss and shadcn UI</p>
-              <p>✅ Guestbook Feature</p>
-              <p>💵 Store Projects in Sanity</p>
-              <p>🐐 Bento Layout</p>
-              <p>😶‍🌫️ Deployment to Vercel</p>
+              {project?.projectFeatures?.map((item, idx) => (
+                <div key={idx}>
+                  <p>{item}</p>
+                </div>
+              ))}
             </div>
           </div>
           <div className="flex-1 grid grid-cols-2 space-x-4">
             <Link
-              href="#"
+              href={project?.repositoryUrl as string}
               className="border border-accent-primary rounded-md p-6 h-[115px] text-center"
             >
               <FaGithub size={36} className="mx-auto" />
               <p className="text-lg mt-2.5">Github</p>
             </Link>
             <Link
-              href="#"
+              href={project?.projectUrl as string}
               className="border border-accent-primary rounded-md p-6 h-[115px] text-center"
             >
               <BiWorld size={36} className="text-red-600 mx-auto" />
-              <p className="text-lg mt-2.5">YouTube</p>
+              <p className="text-lg mt-2.5">Live Link</p>
             </Link>
           </div>
         </div>
