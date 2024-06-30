fdc873988d48f8d60a56784b4ce0675de9d7a3e1
geodevcode <geodevinstitute@gmail.com>
2024-06-30 11:02:35 +0100

    interface for projectlink object added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,21 +4,34 @@ import ProjectsCard from "../components/card/ProjectsCard";
 import { Skeleton } from "../components/skeleton/Skeleton";
 import { useQuery } from "@tanstack/react-query";
 
+
+interface ProjectLink  {
+  id: string;
+  imageUrl: string;
+  projectTitle: string;
+  subTitle: string;
+  stack: string[];
+  repositoryUrl: string;
+  projectUrl: string;
+  projectFeatures: string[];
+};
+
 export default function Projects() {
-  const { data: projectLinksData, isLoading } = useQuery({
+  
+  const { data: projectLinksData, isLoading } = useQuery<ProjectLink[]>({
     queryKey: ["getProjectApi"],
-    queryFn: () => projectLinks,
+    queryFn: () => Promise.resolve(projectLinks),
   });
 
   return (
     <main className="px-4 p-2 md:p-8 min-h-screen mb-6">
       <div className="w-full items-center font-sans gap-6 space-y-6 lg:space-y-0 text-sm p-2 justify-between gap-y-10 lg:grid lg:grid-cols-2 xl:grid-cols-3">
         {isLoading
-          ? projectLinksData?.map((item) => (
-              <div key={item.id}>
-                <Skeleton />
-              </div>
-            ))
+          ? projectLinks.map((_, idx) => (
+            <div key={idx}>
+              <Skeleton />
+            </div>
+          ))
           : projectLinksData?.map((item, idx) => (
               <div key={idx}>
                 <ProjectsCard
