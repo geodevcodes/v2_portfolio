726eed50d9ec8f1f541552da4b053fccd6c6378e
geodevcode <geodevinstitute@gmail.com>
2024-06-30 10:54:52 +0100

    Skeleton added to handle Loading state

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,23 +1,34 @@
+"use client";
 import { projectLinks } from "@/lib/LinkData";
 import ProjectsCard from "../components/card/ProjectsCard";
-// import { Skeleton } from "../components/skeleton/Skeleton";
+import { Skeleton } from "../components/skeleton/Skeleton";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Projects() {
+  const { data: projectLinksData, isLoading } = useQuery({
+    queryKey: ["getProjectApi"],
+    queryFn: () => projectLinks,
+  });
+
   return (
     <main className="px-4 p-2 md:p-8 min-h-screen mb-6">
       <div className="w-full items-center font-sans gap-6 space-y-6 lg:space-y-0 text-sm p-2 justify-between gap-y-10 lg:grid lg:grid-cols-2 xl:grid-cols-3">
-        {projectLinks &&
-          projectLinks.map((item, idx) => (
-            <div key={idx}>
-              <ProjectsCard
-                projectId={item.id}
-                projectTitle={item.projectTitle}
-                subTitle={item.subTitle}
-                cardImageUrl={item.imageUrl}
-              />
-              {/* <Skeleton /> */}
-            </div>
-          ))}
+        {isLoading
+          ? projectLinksData?.map((item) => (
+              <div key={item.id}>
+                <Skeleton />
+              </div>
+            ))
+          : projectLinksData?.map((item, idx) => (
+              <div key={idx}>
+                <ProjectsCard
+                  projectId={item.id}
+                  projectTitle={item.projectTitle}
+                  subTitle={item.subTitle}
+                  cardImageUrl={item.imageUrl}
+                />
+              </div>
+            ))}
       </div>
     </main>
   );
