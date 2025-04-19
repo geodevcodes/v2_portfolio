3475a5c56619104da5d81ccf43384119e3521695
geodevcode <geodevinstitute@gmail.com>
2025-04-19 09:10:42 +0100

    feat: refactor Projects component layout and improve code formatting

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,9 +3,11 @@ import { projectLinks } from "@/lib/LinkData";
 import ProjectsCard from "../components/card/ProjectsCard";
 import { Skeleton } from "../components/skeleton/Skeleton";
 import { useQuery } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { MonitorCheck, Smartphone, } from "lucide-react";
+import Link from "next/link";
 
-
-interface ProjectLink  {
+interface ProjectLink {
   id: string;
   imageUrl: string;
   projectTitle: string;
@@ -14,10 +16,9 @@ interface ProjectLink  {
   repositoryUrl: string;
   projectUrl: string;
   projectFeatures: string[];
-};
+}
 
 export default function Projects() {
-  
   const { data: projectLinksData, isLoading } = useQuery<ProjectLink[]>({
     queryKey: ["getProjectApi"],
     queryFn: () => Promise.resolve(projectLinks),
@@ -25,13 +26,34 @@ export default function Projects() {
 
   return (
     <main className="px-4 p-2 md:p-8 min-h-screen mb-6">
+      <div className="md:flex justify-between items-center pl-3 pb-7 mb-10 border-b border-slate">
+        <p className="text-2xl md:text-3xl mb-5 md:mb-0 lg:text-left lg:text-4xl font-bold">
+          Projects
+        </p>
+        {/* <div className="flex gap-x-6 ">
+          <Button asChild>
+            <Link href="#">
+              <Smartphone className="mr-2 h-4 w-4" />
+              Mobile App
+            </Link>
+          </Button>
+          <Button
+            asChild
+            className="bg-muted text-foreground  hover:bg-muted dark:bg-accent dark:text-secondary-foreground"
+          >
+            <Link href="#">
+              <MonitorCheck className="mr-2 h-4 w-4" /> Web App
+            </Link>
+          </Button>
+        </div> */}
+      </div>
       <div className="w-full items-center font-sans gap-6 space-y-6 lg:space-y-0 text-sm p-2 justify-between gap-y-10 lg:grid lg:grid-cols-2 xl:grid-cols-3">
         {isLoading
           ? projectLinks.map((_, idx) => (
-            <div key={idx}>
-              <Skeleton />
-            </div>
-          ))
+              <div key={idx}>
+                <Skeleton />
+              </div>
+            ))
           : projectLinksData?.map((item, idx) => (
               <div key={idx}>
                 <ProjectsCard
