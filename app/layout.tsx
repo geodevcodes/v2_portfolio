64546725193062948e59da6323dba0b704ac7704
geodevcode <geodevinstitute@gmail.com>
2025-04-19 09:46:30 +0100

    fix: add overflow-hidden to main container in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default async function RootLayout({
             <MobileNavbar />
             <section className="w-full relative flex">
               <SideBar session={session} />
-              <main className="w-full pt-20 md:pt-2 max-w-[1150px] mx-auto xl:max-w-full xl:overflow-x-auto md:ml-[240px] xl:ml-[240px]">
+              <main className="w-full pt-20 md:pt-2 max-w-[1150px] overflow-hidden mx-auto xl:max-w-full xl:overflow-x-auto md:ml-[240px] xl:ml-[240px]">
                 {children}
                 <Footer />
               </main>
