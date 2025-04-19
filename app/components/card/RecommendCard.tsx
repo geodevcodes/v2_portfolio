99ec7cb0b3a442037fc1ee587f2c5463c1ca4501
geodevcode <geodevinstitute@gmail.com>
2025-04-19 09:44:54 +0100

    fix: update image alt text and button label in RecommendCard component

--- app/components/card/RecommendCard.tsx
+++ app/components/card/RecommendCard.tsx
@@ -19,12 +19,15 @@ export default function RecommendCard({
     <div>
       <div className="border border-slate rounded-md">
         <div>
-          <div className="relative rounded-lg w-full h-[180px] md:h-[250px] xl:h-[350px] 2xl:h-[400px]">
+          <div className="rounded-lg w-full h-[180px] md:h-[250px] xl:h-[350px] 2xl:h-[400px]">
             <Image
               src={cardImageUrl}
-              alt="ecommerce prototype image"
-              fill
-              className="rounded-t-md"
+              alt="project banner image"
+              priority
+              width={200}
+              height={200}
+              sizes="(max-width: 768px) 100vw, 700px"
+              className="rounded-t-md w-full h-full object-cover"
             />
           </div>
           <div className="p-4 lg:p-3">
@@ -37,7 +40,7 @@ export default function RecommendCard({
               </p>
               <div>
                 <Button asChild className="w-full mt-4 lg:mt-0">
-                  <Link href={`/projects/${projectId}`}>Watch</Link>
+                  <Link href={`/projects/${projectId}`}>View More...</Link>
                 </Button>
               </div>
             </div>
