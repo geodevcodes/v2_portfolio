8f727fc61a5a0e92337205d7c992d5c273d76aea
geodevcode <geodevinstitute@gmail.com>
2025-04-27 00:45:54 +0100

    fix: clean up BlogHome component by removing unused imports and uncommenting necessary code for blog data fetching

--- app/components/blog/BlogHome.tsx
+++ app/components/blog/BlogHome.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { BlogCard, BlogSmallCard } from "@/app/components/card/BlogCard";
-// import { GetBlogsRequest } from "@/app/services/blog.request";
-import GetStarted from "@/public/get-started.webp";
-// import { useQuery } from "@tanstack/react-query";
+import { GetBlogsRequest } from "@/app/services/blog.request";
+import { useQuery } from "@tanstack/react-query";
 import { ArrowLeft, Plus } from "lucide-react";
-import { blogData } from "@/lib/BlogData";
 import { formatDate } from "@/lib/utils";
-// import CreateBlog from "./CreateBlog";
+import CreateBlog from "./CreateBlog";
 import { useState } from "react";
 import Link from "next/link";
 
@@ -16,10 +14,10 @@ export default function BlogHome() {
   const [currentPage, setCurrentPage] = useState(1);
   const [limit] = useState(6);
 
-  // const { data: blogsData, isLoading } = useQuery({
-  //   queryKey: ["getBlogsApi", currentPage],
-  //   queryFn: () => GetBlogsRequest(currentPage, limit),
-  // });
+  const { data: blogsData } = useQuery({
+    queryKey: ["getBlogsApi", currentPage],
+    queryFn: () => GetBlogsRequest(currentPage, limit),
+  });
 
   return (
     <>
@@ -31,7 +29,10 @@ export default function BlogHome() {
               <span className="text-2xl">Blog</span> Insights for your job
               search journey
             </p>
-            <div className="hidden md:invisible md:block" onClick={() => setShowCreateBlog((prev) => !prev)}>
+            <div
+              className="hidden md:invisible md:block"
+              onClick={() => setShowCreateBlog((prev) => !prev)}
+            >
               {showCreateBlog ? (
                 <div className="my-4 flex p-2 md:p-3 justify-center items-center gap-[8px] rounded-[8px] text-white w-full md:w-[150px] cursor-pointer  px-3.5 py-4 font-light shadow-sm bg-primary">
                   <ArrowLeft className="w-4 h-4 md:w-5 md:h-5" />
@@ -51,27 +52,29 @@ export default function BlogHome() {
           </div>
 
           {showCreateBlog ? (
-            <div className="mt-8 rounded-2xl bg-white border border-slate-200">
-              {/* <CreateBlog setShowCreateBlog={setShowCreateBlog} /> */}
+            <div className="mt-8 rounded-2xl bg-whit border border-slate-200 dark:border-muted">
+              <CreateBlog setShowCreateBlog={setShowCreateBlog} />
             </div>
           ) : (
             <section className="w-full max-w-5xl text-sm">
-              <Link href={`/blog/how-to-land-a-high-paying-remote-job`}>
-                <BlogCard
-                  headings="How to Land a High-Paying Remote Job"
-                  content="Learn the top strategies for landing a high-paying remote job that you actually love."
-                  imageUrl={GetStarted}
-                  createdAt="February 18, 2025"
-                  readTime="7 min read"
-                />
-              </Link>
+              {blogsData?.data?.[0] && (
+                <Link href={`/blog/${blogsData.data[0].slug}`}>
+                  <BlogCard
+                    headings={blogsData.data[0].title}
+                    content={blogsData.data[0].subTitle}
+                    imageUrl={blogsData.data[0].banner}
+                    createdAt={formatDate(blogsData.data[0].createdAt)}
+                    readTime={blogsData.data[0].readTime}
+                  />
+                </Link>
+              )}
               <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-8 py-8 px- rounded-2xl">
-                {blogData?.map((item: any, index: number) => (
+                {blogsData?.data?.map((item: any, index: number) => (
                   <Link href={`/blog/${item.slug}`} key={index}>
                     <BlogSmallCard
-                      headings={item.headings}
-                      content={item.content}
-                      imageUrl={item.imageUrl}
+                      headings={item.title}
+                      content={item.subTitle}
+                      imageUrl={item.banner}
                       createdAt={formatDate(item.createdAt)}
                       readTime={item.readTime}
                     />
