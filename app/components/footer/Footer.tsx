b1a11694817406231b1ac746a4b8566b98eb718b
geodevcode <geodevinstitute@gmail.com>
2025-05-21 16:29:54 +0100

    feat: redesign Footer component with social media links and updated layout

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,25 +1,61 @@
-import { footerLinks } from "@/lib/LinkData";
-import Link from "next/link";
+import {
+  RiGithubFill,
+  RiLinkedinFill,
+  RiTwitterFill,
+  RiDribbbleFill,
+} from "react-icons/ri";
 
 export default function Footer() {
-  // Get Year
-  const getYear = new Date();
-  const dateString = getYear.getFullYear();
   return (
-    <footer className="text-sm bg-background border-t border-slate pt-7 px-6 pb-[36px] border-accent">
-      <div className="lg:flex justify-between items-center">
-        <p className="font-normal text-xs lg:text-sm">
-          © {dateString} geodevcodes
-          <span className="hidden ml-2 xl:inline-block">(Rasheed Olatunde)</span>.
-          All rights reserved.
-        </p>
-        <div className="flex flex-col space-x-0  space-y-3 mt-4  lg:flex-row justify-between lg:space-x-3 lg:space-y-0 lg:mt-0 text-muted-foreground">
-          {footerLinks?.map((item, idx) => (
-            <Link href={`${item.href}`} key={item.key}>
-              {item.name}
-            </Link>
-          ))}
-        </div>
+    <footer className="fixed bottom-0 z-[100000] flex justify-between w-full border-t border-slate bg-background text-[#8CA5B5]">
+      <div className="flex items-center justify-center border-l border-transparent px-4 py-3">
+        <span className="m-0">find me in:</span>
+      </div>
+      <div className="flex items-center justify-center border-l border-slate">
+        <a
+          aria-label="geodevcodes linkedin profile"
+          href="http://linkedin.com/in/olatunderasheed"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center justify-center p-4"
+        >
+          <RiLinkedinFill fill="#607B96" size={25} />
+        </a>
+      </div>
+      <div className="flex items-center justify-center border-l border-slate md:border-r-0">
+        <a
+          aria-label="Qudusayo twitter profile"
+          href="https://twitter.com/geodevcodes"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center justify-center p-4"
+        >
+          <RiTwitterFill fill="#607B96" size={25} />
+        </a>
+      </div>
+      <div className="flex items-center justify-center border-l border-slate border-r ">
+        <a
+          aria-label="geodevcodes github profile"
+          href="https://github.com/olatunde1998/"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center justify-center p-4"
+        >
+          <RiGithubFill fill="#607B96" size={25} />
+        </a>
+      </div>
+      <div className="ml-auto flex items-center justify-center border-l border-slate px-4 py-3">
+        <a
+          aria-label="Designed by geodevcodes"
+          href="https://dribbble.com/shots/18111356-Portfolio-Concept-V-2"
+          target="_blank"
+          rel="noreferrer"
+          title="Designed by geodevcodes"
+          className="flex items-center justify-center"
+        >
+          <span className="mr-2 hidden sm:inline">Designed by geodevcodes</span>
+          <RiDribbbleFill fill="#607B96" size={25} />
+        </a>
       </div>
     </footer>
   );
