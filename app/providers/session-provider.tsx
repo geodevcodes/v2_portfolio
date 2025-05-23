692b96b6b8e01f785d2a6b288feb7ee32905f002
geodevcode <geodevinstitute@gmail.com>
2025-05-23 08:23:58 +0100

    fix: Correct import order for SessionProvider in session-provider.tsx

--- app/providers/session-provider.tsx
+++ app/providers/session-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient();
 const SessionProviderPage = ({ children }: { children: React.ReactNode }) => {
