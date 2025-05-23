ce68026ad5012831172982fd712ba782965aa552
geodevcode <geodevinstitute@gmail.com>
2025-05-23 08:27:17 +0100

    feat: Add LanguageSwitcher component to footer and adjust layout for mobile

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,3 +1,4 @@
+import LanguageSwitcher from "@/app/components/language-switcher/LanguageSwitcher";
 import {
   RiGithubFill,
   RiLinkedinFill,
@@ -8,7 +9,7 @@ import {
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 z-[10] flex justify-between w-full border-t border-slate bg-background text-[#8CA5B5]">
-      <div className="flex items-center justify-center border-l border-transparent px-4 py-3">
+      <div className="hidden md:flex items-center justify-center border-l border-transparent px-4 py-3">
         <span className="m-0">find me in:</span>
       </div>
       <div className="flex items-center justify-center border-l border-slate">
@@ -45,9 +46,9 @@ export default function Footer() {
         </a>
       </div>
       <div className="ml-auto flex items-center justify-center border-l border-slate px-4 py-3">
-        <a
+        {/* <a
           aria-label="Designed by geodevcodes"
-          href="https://dribbble.com/shots/18111356-Portfolio-Concept-V-2"
+          href="#"
           target="_blank"
           rel="noreferrer"
           title="Designed by geodevcodes"
@@ -55,7 +56,16 @@ export default function Footer() {
         >
           <span className="mr-2 hidden sm:inline">Designed by geodevcodes</span>
           <RiDribbbleFill fill="#607B96" size={25} />
-        </a>
+        </a> */}
+        <div className="flex items-center justify-center">
+          <span className="mr-2 hidden sm:inline">Designed by geodevcodes</span>
+          <div className="hidden md:block">
+            <RiDribbbleFill fill="#607B96" size={25} />
+          </div>
+          <div className="md:hidden mb-8">
+            <LanguageSwitcher />
+          </div>
+        </div>
       </div>
     </footer>
   );
