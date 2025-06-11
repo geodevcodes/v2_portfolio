b7aff7d79c50305323c44bef779b00c8f0ce612c
geodevcode <geodevinstitute@gmail.com>
2025-06-11 08:44:59 +0100

    fix: Refactor Profile component to use useSession and update user profile data retrieval

--- app/[locale]/(others)/profile/page.tsx
+++ app/[locale]/(others)/profile/page.tsx
@@ -1,15 +1,24 @@
+import { GetUserProfileRequest } from "@/app/services/users.request";
+import { useQuery } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
-import { auth } from "@/auth";
+import { useSession } from "next-auth/react";
 
-export default async function Profile() {
-  const session = await auth();
-  const fullName = session?.user?.name;
-  const email = session?.user?.email;
-  const profilePicture = session?.user?.image;
+export default function Profile() {
+  const { data: session } = useSession();
+  const token = session?.user?.token as string;
+  const userId = session?.user?._id as string;
+
+  const { data: userProfileData } = useQuery({
+    queryKey: ["getUserProfileApi"],
+    queryFn: () => GetUserProfileRequest(userId, token),
+  });
+
   return (
     <main className="min-h-screen px-4 p-2 md:p-8 md:pt-20 font-sans text-md max-w-7xl">
       <div className="w-full font-sans text-md ">
-        <p className="text-2xl md:text-5xl font-bold mb-2">Settings</p>
+        <p className="text-2xl md:text-3xl lg:text-4xl font-bold mb-2">
+          Settings
+        </p>
         <p className="text-muted-foreground">
           Manage the settings of your account
         </p>
@@ -22,7 +31,7 @@ export default async function Profile() {
             <input
               type="text"
               placeholder="Full name"
-              value={`${fullName}`}
+              value={userProfileData?.data?.fullName}
               className="w-full border border-slate dark:bg-black/30 rounded-sm p-3 focus:outline-none mt-2 text-sm"
             />
           </label>
@@ -34,7 +43,7 @@ export default async function Profile() {
             <span>Email</span>
             <input
               type="email"
-              value={`${email}`}
+              value={userProfileData?.data?.email}
               placeholder="Email"
               disabled
               className="w-full border border-slate dark:bg-black/30 rounded-sm p-3 focus:outline-none mt-2 text-sm text-muted-foreground cursor-not-allowed"
