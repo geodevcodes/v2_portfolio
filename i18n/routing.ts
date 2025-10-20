966f768eeda760a1ec3d833e4ecf312091954a3e
geodevcode <geodevinstitute@gmail.com>
2025-10-20 07:56:49 +0100

    zh-hans changed to zh-CN

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["ko", "de", "en", "ar", "ja", "zh-hans", "fr"],
+  locales: ["ko", "de", "en", "ar", "ja", "zh-CN", "fr"],
 
   // Used when no locale matches
   defaultLocale: "en",
